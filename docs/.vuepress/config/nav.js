6fc358e0f49197598e2a4337fbd06565d8752ec7
imoyao <immoyao@gmail.com>
2020-12-14 22:31:03 +0800

    update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,7 +2,7 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: 'Python',
-    // link: '/Python/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/python/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
