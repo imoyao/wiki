e33349a62285ba9b022d30ae6f72f63781491acf
imoyao <emailme8@163.com>
2021-07-30 14:08:44 +0800

    fix baidupush

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://masantu.com&token=eNOLEFwIoqENSb4M"
 
 rm -rf urls.txt # 灭迹
