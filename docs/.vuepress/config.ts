952f91606313cc071d2138beb25743c2b34e7845
imoyao <emailme8@163.com>
2022-03-17 17:00:49 +0800

    修复评论功能

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -245,11 +245,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: '5dd2493bd694060f4ccb',
+          clientSecret: 'a615756250d2dfeb50e9fbd306774d0563cf55d4',
+          repo: 'gitalk-comment', // GitHub 仓库
+          owner: 'imoyao', // GitHub仓库所有者
+          admin: ['imoyao'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
